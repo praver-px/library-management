3a7b07a46dbbaea97f8cd88703d6609977c2df1f
praver <praverpx@gamil.com>
2022-11-13 19:53:08 +0800

    1.0

---
+++ sql/category.sql
@@ -0,0 +1,7 @@
+INSERT INTO `category`(`id`, `name`, `remark`, `pid`, `createtime`, `updatetime`) VALUES (1, '科幻奇幻类', '科幻奇幻类的小说', NULL, '2022-11-09 10:26:08', '2022-11-09 00:00:00');
+INSERT INTO `category`(`id`, `name`, `remark`, `pid`, `createtime`, `updatetime`) VALUES (2, '历史文化', '历史文化', NULL, '2022-11-09 15:11:47', NULL);
+INSERT INTO `category`(`id`, `name`, `remark`, `pid`, `createtime`, `updatetime`) VALUES (9, '科幻类', '科幻类小说', 1, '2022-11-09 15:39:20', NULL);
+INSERT INTO `category`(`id`, `name`, `remark`, `pid`, `createtime`, `updatetime`) VALUES (10, '都市', NULL, NULL, '2022-11-12 20:01:20', NULL);
+INSERT INTO `category`(`id`, `name`, `remark`, `pid`, `createtime`, `updatetime`) VALUES (11, '异术超能', NULL, 10, '2022-11-12 20:01:49', NULL);
+INSERT INTO `category`(`id`, `name`, `remark`, `pid`, `createtime`, `updatetime`) VALUES (12, '科幻', NULL, NULL, '2022-11-12 20:07:01', NULL);
+INSERT INTO `category`(`id`, `name`, `remark`, `pid`, `createtime`, `updatetime`) VALUES (13, '进化异变', NULL, 12, '2022-11-12 20:07:10', NULL);
